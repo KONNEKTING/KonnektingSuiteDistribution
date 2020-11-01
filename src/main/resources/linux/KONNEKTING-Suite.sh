786c3444dc73af50cccba64714c5a10b48749d8b
Alexander Christian <alex@root1.de>
2020-11-01 16:59:36 +0100

    fixed broken shell file

--- src/main/resources/linux/KONNEKTING-Suite.sh
+++ src/main/resources/linux/KONNEKTING-Suite.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./${jredownload.foldername}/bin/java -cp ./bin/* -splash:splash/splash.png  de.konnekting.suite.Main &
+./jdk-14.0.2+12-jre/bin/java -cp "./bin/*" -splash:splash/splash.png  de.konnekting.suite.Main &
 
