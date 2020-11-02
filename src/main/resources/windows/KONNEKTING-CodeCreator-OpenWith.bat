f54408d89878fd30b6e2e3a2d7f3140ca3f21bd0
Alexander Christian <alex@root1.de>
2020-11-02 13:01:20 +0100

    fixed INI and BAT with correct jar files

--- src/main/resources/windows/KONNEKTING-CodeCreator-OpenWith.bat
+++ src/main/resources/windows/KONNEKTING-CodeCreator-OpenWith.bat
@@ -1,3 +1,4 @@
 @echo off
-start /B .\${jredownload.foldername}\bin\java -cp .\bin\* de.konnekting.codecreator.CodeCreator %1 
+for /f "delims=" %%F in ('dir /b "bin\konnekting-code-creator-*" 2^>nul') do set JAR_FILE=%%F
+start /B .\${jredownload.foldername}\bin\java -cp .\bin\%JAR_FILE% de.konnekting.codecreator.CodeCreator %1 
 
